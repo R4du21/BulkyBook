87f55b67b623ea6cecf211b9664c42a3da906ba4
Raducu <raducu.pescaru@itperspectives.ro>
2022-09-14 21:47:01 +0300

    added Company and all the req modifications for it

---
+++ BulkyBookWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,88 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBookWeb.Controllers;
+[Area("Admin")]
+public class CompanyController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CompanyController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    //GET - Upsert
+    public IActionResult Upsert(int? id)
+    {
+        Company company = new(); //view model for dropdown
+
+        if (id == null || id == 0)
+        {
+            return View(company);
+        }
+        else
+        {
+            company = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
+            return View(company);
+            //update product
+        }
+
+        return View(company);
+    }
+
+    //POST - Upsert
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Upsert(Company obj)
+    {
+        if (ModelState.IsValid)
+        {
+            if (obj.Id == 0) //adding product
+            {
+                _unitOfWork.Company.Add(obj);
+                TempData["success"] = "Company created successfully!";
+
+            }
+            else
+            {
+                _unitOfWork.Company.Update(obj);
+                TempData["success"] = "Company updated successfully!";
+            }
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+
+    #region API CALLS
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var companyList = _unitOfWork.Company.GetAll();
+        return Json(new { data = companyList });
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(int? id)
+    {
+        var obj = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
+        if (obj == null)
+        {
+            return Json(new { succes = false, message = "Error while deleting" });
+        }
+
+        _unitOfWork.Company.Remove(obj);
+        _unitOfWork.Save();
+        return Json(new { succes = true, message = "Delete successful" });
+    }
+    #endregion
+}
