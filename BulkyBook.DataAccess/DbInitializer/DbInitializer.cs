488c2feecb0a664ec66f0de96f450e112ccb9df2
Raducu <raducu.pescaru@itperspectives.ro>
2022-09-19 14:25:35 +0300

    project completed

--- BulkyBook.DataAccess/DbInitializer/DbInitializer.cs
+++ BulkyBook.DataAccess/DbInitializer/DbInitializer.cs
@@ -1,11 +1,14 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BulkyBook.Utility;
+using BulkyBook.Models;
 
 namespace BulkyBook.DataAccess.DbInitializer
 {
@@ -27,10 +30,42 @@ namespace BulkyBook.DataAccess.DbInitializer
         public void Initialize()
         {
             //migrations if they are not applied
-
+            try
+            {
+                if(_db.Database.GetPendingMigrations().Count()>0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception)
+            {
+            }
             //create roles if they are not created
+            if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_User_Indi)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_User_Comp)).GetAwaiter().GetResult();
 
-            //if roles are not created, then we create admin user as well
+                //if roles are not created, then we create admin user as well
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "admin@backend.com",
+                    Email = "admin@backend.com",
+                    Name = "New AdminUser",
+                    PhoneNumber = "07668666877",
+                    StreetAdress = "test 123 Ave",
+                    State = "IL",
+                    PostalCode = "1234",
+                    City = "Chicago"
+                }, "Test2001.").GetAwaiter().GetResult();
+
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "admin@backend.com");
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+            }
+
+            return;
         }
     }
 }
